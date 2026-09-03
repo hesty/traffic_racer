826c894275945e38844f833acf8e3ee4ba471760
Cihat <cihattkaraboga@gmail.com>
2026-09-03 12:59:10 +0300

    Add a separate music toggle to settings with toggle helpers.

--- lib/services/settings_service.dart
+++ lib/services/settings_service.dart
@@ -4,9 +4,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 /// Persists player preferences and exposes them as listenables.
 class SettingsService {
   static const _soundKey = 'sound_enabled';
+  static const _musicKey = 'music_enabled';
   static const _tiltKey = 'tilt_enabled';
 
   final ValueNotifier<bool> soundEnabled = ValueNotifier(true);
+  final ValueNotifier<bool> musicEnabled = ValueNotifier(true);
   final ValueNotifier<bool> tiltEnabled = ValueNotifier(false);
 
   SharedPreferences? _prefs;
@@ -15,19 +17,19 @@ class SettingsService {
     try {
       _prefs = await SharedPreferences.getInstance();
       soundEnabled.value = _prefs!.getBool(_soundKey) ?? true;
+      musicEnabled.value = _prefs!.getBool(_musicKey) ?? true;
       tiltEnabled.value = _prefs!.getBool(_tiltKey) ?? false;
     } catch (_) {
       // Preferences are a convenience; defaults are fine if storage fails.
     }
   }
 
-  void setSound(bool value) {
-    soundEnabled.value = value;
-    _prefs?.setBool(_soundKey, value);
-  }
+  void toggleSound() => _set(soundEnabled, _soundKey, !soundEnabled.value);
+  void toggleMusic() => _set(musicEnabled, _musicKey, !musicEnabled.value);
+  void toggleTilt() => _set(tiltEnabled, _tiltKey, !tiltEnabled.value);
 
-  void setTilt(bool value) {
-    tiltEnabled.value = value;
-    _prefs?.setBool(_tiltKey, value);
+  void _set(ValueNotifier<bool> notifier, String key, bool value) {
+    notifier.value = value;
+    _prefs?.setBool(key, value);
   }
 }
